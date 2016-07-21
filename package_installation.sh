aa21d85114eca6f30d62f5ae5be0d19c633b0564
Manuel Schnitzer <webmaster@mschnitzer.de>
2016-07-21 12:43:56 +0200

    Update package_installation.sh

--- package_installation.sh
+++ package_installation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script installs all needed packages
-zypper in xfce4-terminal dmenu adobe-sourcecodepro-font pulseaudio alsa alsa-plugins-pulses alsa-utils pavucontrol feh
+zypper in xfce4-terminal dmenu adobe-sourcecodepro-font pulseaudio alsa alsa-plugins-pulses alsa-utils pavucontrol feh i3status i3lock
